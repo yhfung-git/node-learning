4f8583d4b84ba2d2fbfb5891dfec590760a013c9
yonghowfung <yhfung940217@gmail.com>
2023-11-15 20:36:07 +0100

    app logic

--- Section_2/assignment/app.js
+++ Section_2/assignment/app.js
@@ -1,5 +1,23 @@
+const path = require("path");
+
 const express = require("express");
 
+const userRoutes = require("./routes/user");
+const mainRoutes = require("./routes/main");
+
 const app = express();
+
+// to handle incoming requests
+app.use(express.urlencoded({ extended: true }));
+
+// to handle static files requests like JS, CSS, Images, etc...
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(userRoutes);
+app.use(mainRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000);
