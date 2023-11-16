8262df64fab4d0cc0066e04bc8fbe7ae75cf8914
yonghowfung <yhfung940217@gmail.com>
2023-11-16 11:37:07 +0100

    added products array to store request body

--- Section_2/learning/routes/admin.js
+++ Section_2/learning/routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require("../utils/path");
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   // console.log("add-product middleware");
@@ -14,8 +16,9 @@ router.get("/add-product", (req, res, next) => {
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  // console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
 module.exports = router;
+module.exports = products;
