d8b6944738627bc24cd7290877d5aff6b7682350
yonghowfung <yhfung940217@gmail.com>
2023-11-16 11:50:41 +0100

    importeed admin.js and console log the products

--- Section_2/learning/routes/shop.js
+++ Section_2/learning/routes/shop.js
@@ -3,11 +3,12 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../utils/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // console.log("main middleware");
+  console.log("shop.js", adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
