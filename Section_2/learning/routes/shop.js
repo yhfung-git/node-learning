f5c97101976d038e80fe86c61b9960436bb12d3b
yonghowfung <yhfung940217@gmail.com>
2023-11-16 23:00:17 +0100

    applied handlebars engine method to the routes

--- Section_2/learning/routes/shop.js
+++ Section_2/learning/routes/shop.js
@@ -8,10 +8,15 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
   res.render("shop", {
-    prods: adminData.products,
+    prods: products,
     pageTitle: "Shop",
     path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
 });
 
