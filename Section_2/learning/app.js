73f6f3a451d500d9c8d768589088e3e36ecef71d
yonghowfung <yhfung940217@gmail.com>
2023-11-16 11:51:42 +0100

    changed name of the required amin and imported the admin routes

--- Section_2/learning/app.js
+++ Section_2/learning/app.js
@@ -4,13 +4,13 @@ const express = require("express");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(express.urlencoded({ extended: true })); // To handle parsing incoming requests
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
