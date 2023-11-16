69183b2e6b59450f27a0f9e5e75002db792b784b
yonghowfung <yhfung940217@gmail.com>
2023-11-16 18:24:07 +0100

    added path key value

--- Section_2/learning/routes/shop.js
+++ Section_2/learning/routes/shop.js
@@ -8,7 +8,11 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("shop", { prods: adminData.products, pageTitle: "Shop" });
+  res.render("shop", {
+    prods: adminData.products,
+    pageTitle: "Shop",
+    path: "/",
+  });
 });
 
 module.exports = router;
