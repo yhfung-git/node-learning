5bf4996109667c7ccf798fbb55dba8b66affdaa1
yonghowfung <yhfung940217@gmail.com>
2023-11-18 21:16:56 +0100

    changed render path

--- Section_3/learning/controllers/products.js
+++ Section_3/learning/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProducts = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formCSS: true,
@@ -16,7 +16,7 @@ exports.postAddProducts = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       pageTitle: "Shop",
       products: products,
       path: "/",
