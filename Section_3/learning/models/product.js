bea0ff3ad1a1348beb850819f0754ee3253b6baa
yonghowfung <yhfung940217@gmail.com>
2023-11-18 20:44:09 +0100

    refactoring code

--- Section_3/learning/models/product.js
+++ Section_3/learning/models/product.js
@@ -4,17 +4,23 @@ const fs = require("fs");
 const dirPath = require("../utils/path");
 const productDataPath = path.join(dirPath, "data", "products.json");
 
+const getProductFromFile = (cb) => {
+  fs.readFile(productDataPath, (err, fileContent) => {
+    if (err) {
+      console.log(err);
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    let products = [];
-    fs.readFile(productDataPath, (err, fileContent) => {
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(productDataPath, JSON.stringify(products), (err) => {
         console.log(err);
@@ -23,12 +29,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    fs.readFile(productDataPath, (err, fileContent) => {
-      if (err) {
-        console.log(err);
-        return cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductFromFile(cb);
   }
 };
