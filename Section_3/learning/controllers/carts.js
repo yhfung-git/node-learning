5045014a411217a194a8d6ec760fbffb8e0d6210
yonghowfung <yhfung940217@gmail.com>
2023-11-19 12:53:56 +0100

    added new controller for cart, checkout

---
+++ Section_3/learning/controllers/carts.js
@@ -0,0 +1,10 @@
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", { pageTitle: "Checkout", path: "/checkout" });
+};
