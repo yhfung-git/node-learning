c7e3c856d237b243fff1a86d78c879a37657105b
yonghowfung <yhfung940217@gmail.com>
2023-11-19 12:53:35 +0100

    created new controller foor admin products

---
+++ Section_3/learning/controllers/adminProducts.js
@@ -0,0 +1,26 @@
+const Product = require("../models/product");
+
+exports.getAddProducts = (req, res, next) => {
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formCSS: true,
+  });
+};
+
+exports.postAddProducts = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/product-list", {
+      pageTitle: "Admin Products",
+      products: products,
+      path: "/admin/products",
+      productCSS: true,
+    });
+  });
+};
