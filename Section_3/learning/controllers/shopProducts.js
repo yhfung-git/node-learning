9a00c888f9e48d0a33ed13861a0ebd51ad4a6e4f
yonghowfung <yhfung940217@gmail.com>
2023-11-19 12:53:00 +0100

    changed name folder

---
+++ Section_3/learning/controllers/shopProducts.js
@@ -0,0 +1,23 @@
+const Product = require("../models/product");
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      pageTitle: "Shop",
+      products: products,
+      path: "/",
+      productCSS: true,
+    });
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      pageTitle: "Your Products",
+      products: products,
+      path: "/products",
+      productCSS: true,
+    });
+  });
+};
