d28bc565c4bb80691ed6cf90a893eac006668756
yonghowfung <yhfung940217@gmail.com>
2023-11-21 19:08:03 +0100

    added postDeleteProduct function to delete a product

--- Section_3/learning/controllers/adminProducts.js
+++ Section_3/learning/controllers/adminProducts.js
@@ -59,3 +59,9 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  Product.deleteById(productId);
+  res.redirect("/admin/product-list");
+};
