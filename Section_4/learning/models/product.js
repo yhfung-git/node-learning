36c22d4ddd2741497039e0b13b08dfd51f028761
yonghowfung <yhfung940217@gmail.com>
2023-11-25 20:22:10 +0100

    imported and using database pool to execute and interact with MySQL by writing SQL commands

--- Section_4/learning/models/product.js
+++ Section_4/learning/models/product.js
@@ -1,20 +1,6 @@
-const path = require("path");
-const fs = require("fs");
-
-const dirPath = require("../utils/path");
-const productDataPath = path.join(dirPath, "data", "products.json");
+const db = require("../utils/database");
 
 const Cart = require("./cart");
-
-const getProductFromFile = (cb) => {
-  fs.readFile(productDataPath, (err, fileContent) => {
-    if (err) {
-      console.log(err);
-      return cb([]);
-    }
-    cb(JSON.parse(fileContent));
-  });
-};
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -26,48 +12,50 @@ module.exports = class Product {
   }
 
   save() {
-    getProductFromFile((products) => {
-      let updatedProductPrice = 0;
-      if (this.id) {
-        const existingProductIndex = products.findIndex((product) => {
-          return product.id === this.id;
-        });
-        const updatedProducts = [...products];
-        const newProductPrice = this.price;
-        const oldProductPrice = updatedProducts[existingProductIndex].price;
-        updatedProductPrice = +(newProductPrice - oldProductPrice);
-        updatedProducts[existingProductIndex] = this;
-        if (updatedProductPrice !== 0) {
-          Cart.updateProductPrice(this.id, updatedProductPrice);
-        }
-        fs.writeFile(
-          productDataPath,
-          JSON.stringify(updatedProducts),
-          (err) => {
-            console.log(err);
-          }
-        );
-      } else {
-        this.id = Date.now().toString();
-        products.push(this);
-        fs.writeFile(productDataPath, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      }
-    });
+    return db.execute(
+      "INSERT INTO products (title, imageUrl, description, price) VALUES (?, ?, ?, ?)",
+      [this.title, this.imageUrl, this.description, this.price]
+    );
   }
 
-  static fetchAll(cb) {
-    getProductFromFile(cb);
+  // save() {
+  //   getProductFromFile((products) => {
+  //     let updatedProductPrice = 0;
+  //     if (this.id) {
+  //       const existingProductIndex = products.findIndex((product) => {
+  //         return product.id === this.id;
+  //       });
+  //       const updatedProducts = [...products];
+  //       const newProductPrice = this.price;
+  //       const oldProductPrice = updatedProducts[existingProductIndex].price;
+  //       updatedProductPrice = +(newProductPrice - oldProductPrice);
+  //       updatedProducts[existingProductIndex] = this;
+  //       if (updatedProductPrice !== 0) {
+  //         Cart.updateProductPrice(this.id, updatedProductPrice);
+  //       }
+  //       fs.writeFile(
+  //         productDataPath,
+  //         JSON.stringify(updatedProducts),
+  //         (err) => {
+  //           console.log(err);
+  //         }
+  //       );
+  //     } else {
+  //       this.id = Date.now().toString();
+  //       products.push(this);
+  //       fs.writeFile(productDataPath, JSON.stringify(products), (err) => {
+  //         console.log(err);
+  //       });
+  //     }
+  //   });
+  // }
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
   }
 
-  static findById(id, cb) {
-    getProductFromFile((products) => {
-      const product = products.find((product) => {
-        return product.id === id;
-      });
-      cb(product);
-    });
+  static findById(id) {
+    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
   }
 
   static deleteById(id) {
