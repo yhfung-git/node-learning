e8267798331ee3a4c446c6859a95ebf75b7e3c7c
yonghowfung <yhfung940217@gmail.com>
2023-11-26 17:43:08 +0100

    added user model

---
+++ Section_4/learning/models/user.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/database");
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
