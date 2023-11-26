475c2c75a8e97b1af2076b64f6f6e39d4b615fff
yonghowfung <yhfung940217@gmail.com>
2023-11-26 17:43:34 +0100

    set up to create a dummy user for learning purpose

--- Section_4/learning/app.js
+++ Section_4/learning/app.js
@@ -9,6 +9,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorsController = require("./controllers/errors");
 const sequelize = require("./utils/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,15 +19,36 @@ app.use(expressLayout);
 app.set("view engine", "ejs");
 app.set("layout", "./layouts/main-layout");
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorsController.error404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
   .sync()
   .then((result) => {
     // console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    return !user
+      ? User.create({ username: "Howard", email: "howard@gmail.com" })
+      : user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
