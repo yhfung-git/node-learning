804b31ff8ecc6d6e8d85a600a2df02846138fd8b
yonghowfung <yhfung940217@gmail.com>
2023-11-27 15:49:37 +0100

    set up associations

--- Section_4/learning/app.js
+++ Section_4/learning/app.js
@@ -11,6 +11,8 @@ const errorsController = require("./controllers/errors");
 const sequelize = require("./utils/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -33,8 +35,16 @@ app.use(shopRoutes);
 
 app.use(errorsController.error404);
 
+// constraints: true = Ensures the product has a userId, onDelete: "CASCADE" = if the user deleted, products with its userId will be deleted as well
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+// constraints: true = Ensures the cart has a userId, onDelete: "CASCADE" = if the user deleted, products with its userId will be deleted as well
+Cart.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+// A cart can have multiple products, cartId will be store in CartItem
+Cart.belongsToMany(Product, { through: CartItem });
+// A product can be in multiple carts, productId will be store in CartItem
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   .sync()
