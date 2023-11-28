e80d64b16dd40f2559be573b7e2f734add74a8d9
yonghowfung <yhfung940217@gmail.com>
2023-11-28 23:04:45 +0100

    set up post create order function

--- Section_4/learning/controllers/orders.js
+++ Section_4/learning/controllers/orders.js
@@ -2,13 +2,20 @@ exports.postCreateOrder = async (req, res, next) => {
   try {
     const cart = await req.user.getCart();
     const products = await cart.getProducts();
+    // create an order
+    const order = await req.user.createOrder();
 
-    res.render("shop/checkout", {
-      pageTitle: "Checkout",
-      path: "/checkout",
-      products: products,
-      productCSS: true,
-    });
+    // Add products to the order
+    await order.addProducts(
+      // Map over the products array and create an array of objects with quantity
+      products.map((product) => {
+        // Set an object with quantity based on the cartItem associated with each product to the orderItem
+        product.orderItem = { quantity: product.cartItem.quantity };
+        return product;
+      })
+    );
+
+    res.redirect("/orders");
   } catch (err) {
     console.log(err);
   }
@@ -20,3 +27,10 @@ exports.getOrders = (req, res, next) => {
     path: "/orders",
   });
 };
+
+// res.render("shop/checkout", {
+//   pageTitle: "Checkout",
+//   path: "/checkout",
+//   products: products,
+//   productCSS: true,
+// });
