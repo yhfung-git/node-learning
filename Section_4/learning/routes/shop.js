48a9446cab71a3ceb47190adf9c744a0b316c387
yonghowfung <yhfung940217@gmail.com>
2023-11-28 23:04:20 +0100

    modified routes for orders

--- Section_4/learning/routes/shop.js
+++ Section_4/learning/routes/shop.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const shopProductsController = require("../controllers/shopProducts");
 const cartsController = require("../controllers/carts");
+const ordersController = require("../controllers/orders");
 
 router.get("/cart", cartsController.getCart);
 
@@ -11,9 +12,9 @@ router.post("/cart", cartsController.postCart);
 
 router.post("/cart-delete-product", cartsController.postCartDeleteProduct);
 
-router.get("/checkout", cartsController.getCheckout);
+router.post("/create-order", ordersController.postCreateOrder);
 
-router.get("/orders", cartsController.getOrders);
+router.get("/orders", ordersController.getOrders);
 
 router.get("/products", shopProductsController.getProducts);
 
