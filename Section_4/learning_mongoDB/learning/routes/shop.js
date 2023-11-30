67df6f832c31c619d455a1c619b4b25ce74641b9
yonghowfung <yhfung940217@gmail.com>
2023-11-30 18:24:23 +0100

    uncommented routes for shop products section

--- Section_4/learning_mongoDB/learning/routes/shop.js
+++ Section_4/learning_mongoDB/learning/routes/shop.js
@@ -6,15 +6,15 @@ const shopProductsController = require("../controllers/shopProducts");
 const cartsController = require("../controllers/carts");
 const ordersController = require("../controllers/orders");
 
-router.get("/cart", cartsController.getCart);
+// router.get("/cart", cartsController.getCart);
 
-router.post("/cart", cartsController.postCart);
+// router.post("/cart", cartsController.postCart);
 
-router.post("/cart-delete-product", cartsController.postCartDeleteProduct);
+// router.post("/cart-delete-product", cartsController.postCartDeleteProduct);
 
-router.post("/create-order", ordersController.postCreateOrder);
+// router.post("/create-order", ordersController.postCreateOrder);
 
-router.get("/orders", ordersController.getOrders);
+// router.get("/orders", ordersController.getOrders);
 
 router.get("/products", shopProductsController.getProducts);
 
