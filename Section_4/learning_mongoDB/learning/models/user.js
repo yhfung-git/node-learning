60a1bc3a0f03935ee02b3d5014c049ea677e3e22
yonghowfung <yhfung940217@gmail.com>
2023-11-30 19:33:36 +0100

    created save and findById a user

--- Section_4/learning_mongoDB/learning/models/user.js
+++ Section_4/learning_mongoDB/learning/models/user.js
@@ -1,23 +1,39 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../utils/database");
+const { getDb } = require("../utils/database");
+const ObjectId = require("mongodb");
 
-const User = sequelize.define("user", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-});
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  async save() {
+    try {
+      const db = getDb();
+      const user = await db.collection("users").insertOne(this);
+
+      if (user.acknowledged && user.insertedId) {
+        return user;
+      }
+    } catch (err) {
+      console.error("Error saving user:", err);
+    }
+  }
+
+  static async findById(id) {
+    try {
+      const db = getDb();
+      const objectId = new ObjectId(id);
+
+      const user = db.collection("users").findOne({ _id: objectId });
+
+      if (user) {
+        return user;
+      }
+    } catch (err) {
+      console.error("Error finding user:", err);
+    }
+  }
+}
 
 module.exports = User;
