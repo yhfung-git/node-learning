36c596f4744916e4b0096e1de862db662185930b
yonghowfung <yhfung940217@gmail.com>
2023-12-01 13:03:33 +0100

    added addToCart method

--- Section_4/learning_mongoDB/learning/models/user.js
+++ Section_4/learning_mongoDB/learning/models/user.js
@@ -2,14 +2,16 @@ const { getDb } = require("../utils/database");
 const { ObjectId } = require("mongodb");
 
 class User {
-  constructor(username, email) {
+  constructor(id, username, email, cart) {
+    this._id = id;
     this.username = username;
     this.email = email;
+    this.cart = cart; // {items: []}
   }
 
   async save() {
     try {
-      const db = getDb();
+      const db = await getDb();
       const user = await db.collection("users").insertOne(this);
 
       if (user.acknowledged && user.insertedId) {
@@ -20,9 +22,43 @@ class User {
     }
   }
 
+  async addToCart(product) {
+    try {
+      const cartProductIndex = await this.cart.items.findIndex((cp) => {
+        // Reference types: Objects, like MongoDB ObjectId instances, may have the same values but not the same address in memory.
+        // To compare their values, use the .equals() method provided by the MongoDB Node.js driver ("mongodb" package).
+        return cp.productId.equals(product._id);
+      });
+
+      const updatedCartItems = [...this.cart.items];
+
+      if (cartProductIndex >= 0) {
+        updatedCartItems[cartProductIndex].quantity += 1;
+      } else {
+        updatedCartItems.push({
+          productId: new ObjectId(product._id),
+          quantity: 1,
+        });
+      }
+
+      const updatedCart = {
+        items: updatedCartItems,
+      };
+
+      const db = await getDb();
+      const objectId = new ObjectId(this._id);
+
+      return await db
+        .collection("users")
+        .updateOne({ _id: objectId }, { $set: { cart: updatedCart } });
+    } catch (err) {
+      console.error("Error adding product to cart:", err);
+    }
+  }
+
   static async findById(id) {
     try {
-      const db = getDb();
+      const db = await getDb();
       const objectId = new ObjectId(id);
 
       const user = db.collection("users").findOne({ _id: objectId });
