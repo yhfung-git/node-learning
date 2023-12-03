7fe0e0fd51b56631d1b073870d911c9f91ae14da
yonghowfung <yhfung940217@gmail.com>
2023-12-03 20:20:43 +0100

    added userId

--- Section_4/learning_mongoDB/mongoose/models/product.js
+++ Section_4/learning_mongoDB/mongoose/models/product.js
@@ -18,6 +18,11 @@ const productSchema = new Schema({
     type: Number,
     required: true,
   },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
