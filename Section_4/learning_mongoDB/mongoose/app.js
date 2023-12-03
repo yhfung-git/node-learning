a77226470fb5690194b0ebe09ca710ea3998127e
yonghowfung <yhfung940217@gmail.com>
2023-12-03 21:08:06 +0100

    implement a create and check existing user method

--- Section_4/learning_mongoDB/mongoose/app.js
+++ Section_4/learning_mongoDB/mongoose/app.js
@@ -2,13 +2,16 @@ const path = require("path");
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
+
+require("dotenv").config();
+const url = process.env.MONGODB_URL;
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorsController = require("./controllers/errors");
-const { mongoConnect } = require("./utils/database");
 const User = require("./models/user");
 
 app.use(express.urlencoded({ extended: false }));
@@ -20,8 +23,8 @@ app.set("layout", "./layouts/main-layout");
 
 app.use(async (req, res, next) => {
   try {
-    const user = await User.findById("656a0d5c62af0fb7a3e25fff");
-    req.user = new User(user._id, user.username, user.email, user.cart);
+    const user = await User.findOne();
+    req.user = user;
     next();
   } catch (err) {
     console.log(err);
@@ -35,7 +38,18 @@ app.use(errorsController.error404);
 
 (async () => {
   try {
-    await mongoConnect();
+    await mongoose.connect(url);
+
+    const existingUser = await User.findOne();
+    if (!existingUser) {
+      const newUser = new User({
+        username: "Howard",
+        email: "howard@gmail.com",
+        cart: { items: [] },
+      });
+      await newUser.save();
+    }
+
     app.listen(3000, () => {
       console.log("Server is running on port 3000!");
     });
