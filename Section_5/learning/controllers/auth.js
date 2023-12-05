fa7d0759b73f01e5c98599870e15d674c14eabba
yonghowfung <yhfung940217@gmail.com>
2023-12-05 19:41:15 +0100

    addd postLogin function

--- Section_5/learning/controllers/auth.js
+++ Section_5/learning/controllers/auth.js
@@ -5,8 +5,18 @@ exports.getLogin = async (req, res, next) => {
       path: "/login",
       formCSS: true,
       authCSS: true,
+      isLoggedIn: req.isLoggedIn,
     });
   } catch (err) {
-    console.log(err);
+    console.log("get login error:", err);
+  }
+};
+
+exports.postLogin = async (req, res, next) => {
+  try {
+    req.isLoggedIn = true;
+    res.redirect("/");
+  } catch (err) {
+    console.log("post login error:", err);
   }
 };
