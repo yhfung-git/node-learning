e0aba0c28633b407b8fce2d7b880d791b6e79624
yonghowfung <yhfung940217@gmail.com>
2023-12-05 18:56:21 +0100

    added auth contoller

---
+++ Section_5/learning/controllers/auth.js
@@ -0,0 +1,12 @@
+exports.getLogin = async (req, res, next) => {
+  try {
+    res.render("auth/login", {
+      pageTitle: "Login",
+      path: "/login",
+      formCSS: true,
+      authCSS: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
