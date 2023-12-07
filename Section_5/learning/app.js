682bebfb8ccd9fd95a34297f63c38c8a1796b41a
yonghowfung <yhfung940217@gmail.com>
2023-12-07 13:35:23 +0100

    added cookie example on session

--- Section_5/learning/app.js
+++ Section_5/learning/app.js
@@ -39,7 +39,12 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: store,
-    // cookie: { httpOnly: true, secure: true },
+    // cookie: {
+    //   httpOnly: true,
+    //   secure: true,
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    //   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    // },
   })
 );
 
