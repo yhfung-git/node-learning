763d1be1e5152d4db91ff813d8bb85297f671109
yonghowfung <yhfung940217@gmail.com>
2023-12-08 21:43:20 +0100

    added isAuth to the routes need to proctct

--- Section_5/learning/routes/shop.js
+++ Section_5/learning/routes/shop.js
@@ -5,16 +5,21 @@ const router = express.Router();
 const shopController = require("../controllers/shop");
 const cartsController = require("../controllers/carts");
 const ordersController = require("../controllers/orders");
+const isAuth = require("../middleware/is-auth");
 
-router.get("/cart", cartsController.getCart);
+router.get("/cart", isAuth, cartsController.getCart);
 
-router.post("/cart", cartsController.postCart);
+router.post("/cart", isAuth, cartsController.postCart);
 
-router.post("/cart-delete-product", cartsController.postCartDeleteProduct);
+router.post(
+  "/cart-delete-product",
+  isAuth,
+  cartsController.postCartDeleteProduct
+);
 
-router.post("/create-order", ordersController.postCreateOrder);
+router.post("/create-order", isAuth, ordersController.postCreateOrder);
 
-router.get("/orders", ordersController.getOrders);
+router.get("/orders", isAuth, ordersController.getOrders);
 
 router.get("/products", shopController.getProducts);
 
