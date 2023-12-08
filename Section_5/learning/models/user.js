35efdf42c27143a5ca4f57d93d08f86fec8be8b8
yonghowfung <yhfung940217@gmail.com>
2023-12-08 18:22:43 +0100

    modified the user model

--- Section_5/learning/models/user.js
+++ Section_5/learning/models/user.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
     required: true,
+    unique: true,
   },
-  email: {
+  password: {
     type: String,
     required: true,
   },
