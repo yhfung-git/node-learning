963a5d57d7080946467563baf51f501d961923ef
yonghowfung <yhfung940217@gmail.com>
2023-12-11 18:04:34 +0100

    added empty object to avoid undefined

--- Section_5/learning/middleware/alerts.js
+++ Section_5/learning/middleware/alerts.js
@@ -1,11 +1,15 @@
 module.exports = (req, res, next) => {
-  if (req.user) {
-    res.locals.alerts = {
-      error: req.flash("error"),
-      success: req.flash("success"),
-      warning: req.flash("warning"),
-      info: req.flash("info"),
-    };
+  if (!req.user) {
+    res.locals.alerts = {};
+    return next();
   }
+
+  res.locals.alerts = {
+    error: req.flash("error"),
+    success: req.flash("success"),
+    warning: req.flash("warning"),
+    info: req.flash("info"),
+  };
+
   next();
 };
