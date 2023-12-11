a84ef155e4543c0504d1614e7b2a4520f7ecacd0
yonghowfung <yhfung940217@gmail.com>
2023-12-11 17:50:41 +0100

    added req.user to prevent alerts create session when there is no user logged in

--- Section_5/learning/middleware/alerts.js
+++ Section_5/learning/middleware/alerts.js
@@ -1,9 +1,11 @@
 module.exports = (req, res, next) => {
-  res.locals.alerts = {
-    error: req.flash("error"),
-    success: req.flash("success"),
-    warning: req.flash("warning"),
-    info: req.flash("info"),
-  };
+  if (req.user) {
+    res.locals.alerts = {
+      error: req.flash("error"),
+      success: req.flash("success"),
+      warning: req.flash("warning"),
+      info: req.flash("info"),
+    };
+  }
   next();
 };
