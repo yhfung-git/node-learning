3992b5cab129d849fda6c97951108bbc191ac075
yonghowfung <yhfung940217@gmail.com>
2023-12-13 21:21:44 +0100

    added get reset password route

--- Section_5/learning/routes/auth.js
+++ Section_5/learning/routes/auth.js
@@ -14,4 +14,6 @@ router.get("/signup", authController.getSignup);
 
 router.post("/signup", authController.postSignup);
 
+router.get("/reset-password", authController.getResetPassword);
+
 module.exports = router;
