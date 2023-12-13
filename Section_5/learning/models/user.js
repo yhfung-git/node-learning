77dd614aa2bdf3ad6d9e4918a0ee8ac7b8385b34
yonghowfung <yhfung940217@gmail.com>
2023-12-13 16:51:03 +0100

    added first last name and username field

--- Section_5/learning/models/user.js
+++ Section_5/learning/models/user.js
@@ -2,6 +2,19 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
