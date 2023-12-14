da9404871e18ccf29c4dc055fa268a823b3a196c
yonghowfung <yhfung940217@gmail.com>
2023-12-14 21:36:18 +0100

    redirect if user is non authenticated and authenticated user trying to access to admin page

--- Section_5/learning/middleware/is-auth.js
+++ Section_5/learning/middleware/is-auth.js
@@ -1,10 +1,24 @@
-module.exports = async (req, res, next) => {
-  try {
+module.exports = (roles) => {
+  return (req, res, next) => {
     if (!req.session.user) {
+      req.flash("error", "Please log in before accessing the page");
       return res.redirect("/login");
     }
-    next();
-  } catch (err) {
-    console.log("Error Authenticating:", err);
-  }
+
+    if (
+      req.session.user &&
+      req.user.role === "user" &&
+      req.originalUrl.startsWith("/admin")
+    ) {
+      req.flash(
+        "error",
+        "Oops! It looks like you need admin privileges to access this page"
+      );
+      return res.redirect("/");
+    }
+
+    if (req.session.user && roles.includes(req.user.role)) {
+      return next();
+    }
+  };
 };
