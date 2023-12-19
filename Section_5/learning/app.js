194dbfa684aff86381bbb2292756c8a9f7524aa0
yonghowfung <yhfung940217@gmail.com>
2023-12-19 11:42:51 +0100

    changed session maxAge from 24 hours to 1 hour

--- Section_5/learning/app.js
+++ Section_5/learning/app.js
@@ -54,7 +54,7 @@ app.use(
     cookie: {
       httpOnly: true,
       //   secure: true,
-      maxAge: 24 * 60 * 60 * 1000,
+      maxAge: 60 * 60 * 1000,
       //   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     },
   })
