70b1964907d8e9a288d895676403be583ee81050
yonghowfung <yhfung940217@gmail.com>
2023-12-19 12:54:31 +0100

    use checkProductInput validation middleware

--- Section_5/learning/routes/admin.js
+++ Section_5/learning/routes/admin.js
@@ -3,17 +3,21 @@ const express = require("express");
 const router = express.Router();
 
 const adminController = require("../controllers/admin");
-const { checkAddProduct } = require("../utils/validators");
+const { checkProductInput } = require("../utils/validators");
 
 router.get("/product-list", adminController.getProducts);
 
 router.get("/add-product", adminController.getAddProduct);
 
-router.post("/add-product", checkAddProduct, adminController.postAddProduct);
+router.post("/add-product", checkProductInput, adminController.postAddProduct);
 
 router.get("/edit-product/:productId", adminController.getEditProduct);
 
-router.post("/edit-product", adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  checkProductInput,
+  adminController.postEditProduct
+);
 
 router.post("/delete-product", adminController.postDeleteProduct);
 
