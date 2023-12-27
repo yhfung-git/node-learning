c1dfbe5197ae90c9790432285d4fbc9a4385a856
yonghowfung <yhfung940217@gmail.com>
2023-12-27 21:30:47 +0100

    set up with flash message

--- Section_6/learning/public/js/admin.js
+++ Section_6/learning/public/js/admin.js
@@ -1,13 +1,32 @@
-const deleteProduct = (btn) => {
-  const productId = btn.parentNode.querySelector("[name=productId]").value;
-  const csrfToken = btn.parentNode.querySelector("[name=csrfToken]").value;
+import { displayFlashMessage } from "./flash-messages.js";
 
-  fetch(`/admin/product/${productId}`, {
-    method: "DELETE",
-    headers: {
-      "csrf-token": csrfToken,
-    },
-  })
-    .then((result) => console.log("Product deleted successfully:", result))
-    .catch((err) => console.log("Error deleting product:", err));
+const deleteProduct = async (btn) => {
+  try {
+    const productId = btn.parentNode.querySelector("[name=productId]").value;
+    const csrfToken = btn.parentNode.querySelector("[name=csrfToken]").value;
+
+    const productElement = btn.closest("article");
+
+    const response = await fetch(`/admin/product/${productId}`, {
+      method: "DELETE",
+      headers: {
+        "csrf-token": csrfToken,
+      },
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      displayFlashMessage("error", result.error);
+      throw new Error(`Error deleting product: ${response.status}`);
+    }
+
+    productElement.parentNode.removeChild(productElement);
+
+    displayFlashMessage("success", result.message);
+  } catch (err) {
+    throw err;
+  }
 };
+
+window.deleteProduct = deleteProduct;
