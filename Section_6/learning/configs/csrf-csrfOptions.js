2c08106c617c05970c9bdfd2439ceb6f283d693e
yonghowfung <yhfung940217@gmail.com>
2023-12-27 19:05:04 +0100

    added if else to handle csrf headers

--- Section_6/learning/configs/csrf-csrfOptions.js
+++ Section_6/learning/configs/csrf-csrfOptions.js
@@ -7,7 +7,12 @@ module.exports = {
     cookieOptions: {
       sameSite: "strict",
     },
-    getTokenFromRequest: (req) => req.body.csrfToken,
+    getTokenFromRequest: (req) => {
+      if (!/delete/i.test(req.method)) {
+        return req.body.csrfToken;
+      }
+      return req.headers["csrf-token"];
+    },
   },
 };
 
