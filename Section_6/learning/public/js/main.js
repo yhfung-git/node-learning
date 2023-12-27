b43776fd5f51dfb2b0f37745d4aa78675f95619c
yonghowfung <yhfung940217@gmail.com>
2023-12-27 20:48:15 +0100

    moved mobile nav code to another file and import in here

--- Section_6/learning/public/js/main.js
+++ Section_6/learning/public/js/main.js
@@ -1,19 +1,4 @@
-const backdrop = document.querySelector(".backdrop");
-const sideDrawer = document.querySelector(".mobile-nav");
-const menuToggle = document.querySelector("#side-menu-toggle");
-
-function backdropClickHandler() {
-  backdrop.style.display = "none";
-  sideDrawer.classList.remove("open");
-}
-
-function menuToggleClickHandler() {
-  backdrop.style.display = "block";
-  sideDrawer.classList.add("open");
-}
-
-backdrop.addEventListener("click", backdropClickHandler);
-menuToggle.addEventListener("click", menuToggleClickHandler);
+import "./mobile-nav.js";
 
 const logout = () => {
   document.getElementById("logoutForm").submit();
