0fa30840eb018ddd37a296fae93462ec7e57787c
yonghowfung <yhfung940217@gmail.com>
2024-01-02 20:57:40 +0100

    added post info in both get and post request

--- Section_7/apis_learning/controllers/feed.js
+++ Section_7/apis_learning/controllers/feed.js
@@ -2,8 +2,16 @@ exports.getPosts = async (req, res, next) => {
   try {
     res.status(200).json({
       posts: [
-        { title: "First Post", content: "This is the FIRST post!" },
-        { title: "Second Post", content: "This is the SECOND post!" },
+        {
+          _id: Date.now(),
+          title: "First Post",
+          content: "This is the FIRST post!",
+          imageUrl: "images/cards.jpg",
+          creator: {
+            name: "Howard",
+          },
+          createdAt: new Date(),
+        },
       ],
     });
   } catch (err) {
@@ -16,7 +24,13 @@ exports.createPost = async (req, res, next) => {
     const { title, content } = req.body;
     res.status(201).json({
       message: "Post created successfully!",
-      post: { id: Date.now(), title, content },
+      post: {
+        _id: Date.now(),
+        title,
+        content,
+        creator: { name: "Howard" },
+        createdAt: new Date(),
+      },
     });
   } catch (err) {
     console.error(err);
