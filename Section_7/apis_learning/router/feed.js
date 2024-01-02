fc3fecc11b8e61b9ec68d5b3d46deb36b0e65b2a
yonghowfung <yhfung940217@gmail.com>
2024-01-02 21:35:05 +0100

    use checkCreatePostInput validator on post request

--- Section_7/apis_learning/router/feed.js
+++ Section_7/apis_learning/router/feed.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const feedController = require("../controllers/feed");
+const { checkCreatePostInput } = require("../helpers/validators");
 
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
 
-router.post("/create-post", feedController.createPost);
+router.post("/create-post", checkCreatePostInput, feedController.createPost);
 
 module.exports = router;
