41f7706bf56ce29d46ebe47976dbb0ad6750cbd3
yonghowfung <yhfung940217@gmail.com>
2024-01-03 00:57:27 +0100

    use FormData instead of json (file + data)

--- Section_7/rest_apis_learning_latest/src/pages/Feed/Feed.jsx
+++ Section_7/rest_apis_learning_latest/src/pages/Feed/Feed.jsx
@@ -104,7 +104,12 @@ const Feed = () => {
 
   const finishEditHandler = (postData) => {
     setEditLoading(true);
-    // Set up data (with image!)
+
+    const formData = new FormData();
+    formData.append("title", postData.title);
+    formData.append("content", postData.content);
+    formData.append("image", postData.image);
+
     let url = "http://localhost:8080/feed/create-post";
     let method = "POST";
 
@@ -114,13 +119,7 @@ const Feed = () => {
 
     fetch(url, {
       method: method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: postData.title,
-        content: postData.content,
-      }),
+      body: formData,
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
