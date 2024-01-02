bb604cd265c5b856248f231b5c0424387c3a5315
yonghowfung <yhfung940217@gmail.com>
2024-01-02 21:34:23 +0100

    added validators for validate input

---
+++ Section_7/apis_learning/helpers/validators.js
@@ -0,0 +1,24 @@
+const { body } = require("express-validator");
+
+exports.checkCreatePostInput = [
+  body("title")
+    .trim()
+    .exists({ checkFalsy: true })
+    .withMessage("You must enter a title")
+    .matches(/^[a-zA-Z0-9\s]+$/) // letters, numbers and whitespace
+    .withMessage("Title must only contain letters and numbers")
+    .isLength({ min: 5 })
+    .withMessage("You must enter at least 5 characters"),
+  body("content")
+    .trim()
+    .exists({ checkFalsy: true })
+    .withMessage("You must enter a content")
+    .isLength({ min: 5 })
+    .withMessage("You must enter at least 5 characters"),
+  // body("image").custom((value, { req }) => {
+  //   if (!req.file || !req.file.mimetype.startsWith("image")) {
+  //     throw new Error("No image found or uploaded file is not an image");
+  //   }
+  //   return true;
+  // }),
+];
