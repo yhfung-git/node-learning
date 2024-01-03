a669170cdd42d8ee89ee7d14fe41ddd46301ef5f
yonghowfung <yhfung940217@gmail.com>
2024-01-03 17:32:18 +0100

    added user model

---
+++ Section_7/apis_learning/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    status: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
