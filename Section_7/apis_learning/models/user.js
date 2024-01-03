1431d899276686f9d46913c85ee4e6ffa75e8a86
yonghowfung <yhfung940217@gmail.com>
2024-01-03 19:09:31 +0100

    changed status from required to default

--- Section_7/apis_learning/models/user.js
+++ Section_7/apis_learning/models/user.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema(
   {
     name: { type: String, required: true, unique: true },
-    status: { type: String, required: true },
+    status: { type: String, default: "New user" },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
