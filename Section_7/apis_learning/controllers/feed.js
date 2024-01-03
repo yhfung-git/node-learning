0c27c847f38a26e72b69d641edf4ccdf291ea792
yonghowfung <yhfung940217@gmail.com>
2024-01-03 15:42:55 +0100

    added deletePost function

--- Section_7/apis_learning/controllers/feed.js
+++ Section_7/apis_learning/controllers/feed.js
@@ -94,7 +94,7 @@ exports.updatePost = async (req, res, next) => {
       throw error;
     }
 
-    if (imageUrl !== post.imageUrl) clearImage(post.imageUrl);
+    if (imageUrl !== post.imageUrl) await clearImage(post.imageUrl);
 
     post.set({ title, content, imageUrl });
 
@@ -108,6 +108,33 @@ exports.updatePost = async (req, res, next) => {
     res.status(200).json({
       message: "Post updated successfully!",
       post: updatedPost,
+    });
+  } catch (err) {
+    err.statusCode = err?.statusCode ?? 500;
+    next(err);
+  }
+};
+
+exports.deletePost = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      const error = errorHandler(404, "Post not found");
+      throw error;
+    }
+
+    await clearImage(post.imageUrl);
+
+    const deletedPost = await Post.findByIdAndDelete(postId);
+    if (!deletedPost) {
+      const error = errorHandler(500, "Failed to delete the post");
+      throw error;
+    }
+
+    res.status(200).json({
+      message: "Post deleted successfully!",
     });
   } catch (err) {
     err.statusCode = err?.statusCode ?? 500;
