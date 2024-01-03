f4f49987697ae1caad2bfcd6776ce5ee09ff86df
yonghowfung <yhfung940217@gmail.com>
2024-01-03 22:55:14 +0100

    added put signup route

--- Section_7/apis_learning/routes/auth.js
+++ Section_7/apis_learning/routes/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
 const authController = require("../controllers/auth");
+const { checkSignupInput } = require("../helpers/validators");
 
 const router = express.Router();
+
+router.put("/signup", checkSignupInput, authController.signup);
 
 module.exports = router;
