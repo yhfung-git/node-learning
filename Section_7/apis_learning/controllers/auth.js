cf5937a6c6986d5cff3a70d43e3b51603649d5c7
yonghowfung <yhfung940217@gmail.com>
2024-01-04 17:58:00 +0100

    created login function and use JWT

--- Section_7/apis_learning/controllers/auth.js
+++ Section_7/apis_learning/controllers/auth.js
@@ -1,4 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { JWT_PRIVATE_KEY } = process.env;
 
 const { handleValidationErrors } = require("../middlewares/validation");
 const { errorHandler } = require("../utils/errorHandler");
@@ -14,10 +17,7 @@ exports.signup = async (req, res, next) => {
     const saltRounds = 12;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    if (!hashedPassword) {
-      const error = errorHandler(500, "Failed to register user");
-      throw error;
-    }
+    if (!hashedPassword) throw errorHandler(500, "Failed to register user");
 
     const newUser = new User({
       name,
@@ -26,15 +26,35 @@ exports.signup = async (req, res, next) => {
     });
 
     const newUserSaved = await newUser.save();
-    if (!newUserSaved) {
-      const error = errorHandler(500, "Failed to register user");
-      throw error;
-    }
+    if (!newUserSaved) throw errorHandler(500, "Failed to register user");
 
     res.status(201).json({
       message: "Congratulations! You've successfully signed up!",
       userId: newUserSaved._id,
     });
+  } catch (err) {
+    err.statusCode = err?.statusCode ?? 500;
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) throw errorHandler(401, "Invalid email or password");
+
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) throw errorHandler(401, "Invalid email or password");
+
+    const userId = user._id.toString();
+    const token = jwt.sign({ email: user.email, userId }, JWT_PRIVATE_KEY, {
+      expiresIn: "1h",
+      algorithm: "HS256",
+    });
+
+    res.status(200).json({ token, userId });
   } catch (err) {
     err.statusCode = err?.statusCode ?? 500;
     next(err);
