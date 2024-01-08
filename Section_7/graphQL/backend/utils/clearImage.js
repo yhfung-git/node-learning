7f2038118f72964dd04b7e861832fb5ea0043240
yonghowfung <yhfung940217@gmail.com>
2024-01-08 23:02:32 +0100

    modified error handling in clearImage catch block

--- Section_7/graphQL/backend/utils/clearImage.js
+++ Section_7/graphQL/backend/utils/clearImage.js
@@ -1,8 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
-
 const rootDir = require("./path");
-const { errorHandler } = require("./errorHandler");
 
 exports.clearImage = async (filePath) => {
   try {
@@ -11,9 +9,7 @@ exports.clearImage = async (filePath) => {
     await fs.unlink(absolutePath);
     return true;
   } catch (err) {
-    const errorMessage =
-      err.message || "Image not found or failed to delete the image";
-
-    throw errorHandler(500, errorMessage);
+    console.error("clear image error:", err);
+    throw err;
   }
 };
