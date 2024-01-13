dbbacdab25bd46a3112edc81994d664c90c01e65
yonghowfung <yhfung940217@gmail.com>
2024-01-14 00:41:24 +0100

    addded upload to cloudinary and extract cloudinary image id function

--- Section_8/shop_app/utils/file-helper.js
+++ Section_8/shop_app/utils/file-helper.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const cloudinary = require("cloudinary").v2;
 const rootDir = require("./path");
 
 exports.deleteFile = (filePath) => {
@@ -7,5 +8,35 @@ exports.deleteFile = (filePath) => {
 
   fs.unlink(absolutePath, (err) => {
     if (err) throw err;
+  });
+};
+
+exports.uploadImageToCloudinary = async (image) => {
+  try {
+    return new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        { resource_type: "image" },
+        (err, result) => {
+          err ? reject(err) : resolve(result.secure_url);
+        }
+      );
+
+      stream.end(image.buffer);
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.extractPublicId = async (imageUrl) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const parts = imageUrl.split("/");
+      const filename = parts[parts.length - 1];
+      const publicId = filename.split(".")[0];
+      resolve(publicId);
+    } catch (err) {
+      reject(err);
+    }
   });
 };
