67bf9f320ee04827b66e48ee2a865ab877371344
yonghowfung <yhfung940217@gmail.com>
2024-01-17 13:30:52 +0100

    learned working with Union Types

--- Section_8/typescript/app.js
+++ Section_8/typescript/app.js
@@ -2,10 +2,21 @@
 const num1Element = document.getElementById("num1");
 const num2Element = document.getElementById("num2");
 const buttonElement = document.querySelector("button");
-const add = (num1, num2) => num1 + num2;
-buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener("click", () => {
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    const sum = add(+num1, +num2);
-    console.log(sum);
-});
+const add = (num1, num2) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return `${num1} --- ${num2}`;
+  }
+  return +num1 + +num2;
+};
+buttonElement === null || buttonElement === void 0
+  ? void 0
+  : buttonElement.addEventListener("click", () => {
+      const num1 = num1Element.value;
+      const num2 = num2Element.value;
+      const sum = add(+num1, +num2);
+      const string = add(num1, num2);
+      console.log(sum);
+      console.log(string);
+    });
--- Section_8/typescript/app.ts
+++ Section_8/typescript/app.ts
@@ -2,11 +2,21 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button");
 
-const add = (num1: number, num2: number) => num1 + num2;
+const add = (num1: number | string, num2: number | string): number | string => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return `${num1} --- ${num2}`;
+  }
+  return +num1 + +num2;
+};
 
 buttonElement?.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const sum = add(+num1, +num2);
+  const string = add(num1, num2);
+
   console.log(sum);
+  console.log(string);
 });
