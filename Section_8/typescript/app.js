89a9de4b8a23a82c41ea01686fd9b0a81192681b
yonghowfung <yhfung940217@gmail.com>
2024-01-17 14:00:55 +0100

    learned Object & Array Types

--- Section_8/typescript/app.js
+++ Section_8/typescript/app.js
@@ -2,21 +2,29 @@
 const num1Element = document.getElementById("num1");
 const num2Element = document.getElementById("num2");
 const buttonElement = document.querySelector("button");
+const numResults = [];
+const textResults = [];
 const add = (num1, num2) => {
-  if (typeof num1 === "number" && typeof num2 === "number") {
-    return num1 + num2;
-  } else if (typeof num1 === "string" && typeof num2 === "string") {
-    return `${num1} --- ${num2}`;
-  }
-  return +num1 + +num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return `${num1} + ${num2}`;
+    }
+    return +num1 + +num2;
 };
-buttonElement === null || buttonElement === void 0
-  ? void 0
-  : buttonElement.addEventListener("click", () => {
-      const num1 = num1Element.value;
-      const num2 = num2Element.value;
-      const sum = add(+num1, +num2);
-      const string = add(num1, num2);
-      console.log(sum);
-      console.log(string);
-    });
+const printResult = (resultObj) => {
+    console.log(resultObj);
+    console.log(resultObj.value);
+    console.log(resultObj.timestamp);
+};
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener("click", () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const sum = add(+num1, +num2);
+    const string = add(num1, num2);
+    numResults.push(sum);
+    textResults.push(string);
+    printResult({ value: sum, timestamp: new Date() });
+    console.log(numResults, textResults);
+});
--- Section_8/typescript/app.ts
+++ Section_8/typescript/app.ts
@@ -2,21 +2,35 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button");
 
+const numResults: number[] = [];
+const textResults: string[] = [];
+
 const add = (num1: number | string, num2: number | string): number | string => {
   if (typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2;
   } else if (typeof num1 === "string" && typeof num2 === "string") {
-    return `${num1} --- ${num2}`;
+    return `${num1} + ${num2}`;
   }
   return +num1 + +num2;
+};
+
+const printResult = (resultObj: { value: number; timestamp: Date }) => {
+  console.log(resultObj);
+  console.log(resultObj.value);
+  console.log(resultObj.timestamp);
 };
 
 buttonElement?.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
+
   const sum = add(+num1, +num2);
   const string = add(num1, num2);
 
-  console.log(sum);
-  console.log(string);
+  numResults.push(sum as number);
+  textResults.push(string as string);
+
+  printResult({ value: sum as number, timestamp: new Date() });
+
+  console.log(numResults, textResults);
 });
